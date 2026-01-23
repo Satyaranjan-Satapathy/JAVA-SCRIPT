73ba70aa86fd93a778b8b4ff2568e233c9b9f81b
Satyaranjan-Satapathy <satyaranjansatapathy48@gmail.com>
2026-01-23 13:33:53 +0000

    done

---
+++ .devcontainer/basics/conversion operation.js
@@ -0,0 +1,29 @@
+let score = "satay"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"22"=>22
+//"22asd"=>NaN(not a number)
+//true => 1; false => 0
+
+
+let isLoggedIn = "satya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true;0 => false
+//"" => false
+//"satya" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//
