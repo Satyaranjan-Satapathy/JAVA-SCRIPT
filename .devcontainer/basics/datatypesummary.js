e5a471cb264b57b47173a1dd67ff41a9eb170a3d
Satyaranjan-Satapathy <satyaranjansatapathy48@gmail.com>
2026-01-25 04:52:15 +0000

    datatype done

---
+++ .devcontainer/basics/datatypesummary.js
@@ -0,0 +1,28 @@
+//primitive datatype
+// 7 type: string,number, Bollean, Null, Undefined, Symbol, Bigint
+
+const Id= Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(Id === anotherId);
+
+const bigNumber = 132564845415313486441345848n
+
+
+
+//Reference type(Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name:"satya",
+    age: 22,
+}
+
+/*function myFunction = function(){
+    console.log("hello world");
+    
+}*/
+
+console.log(typeof bigNUmber);
