f72f5771ecdc4aad42cde8001cffdf70e2ebaecd
Satyaranjan-Satapathy <satyaranjansatapathy48@gmail.com>
2026-01-26 12:12:07 +0000

    dates done

---
+++ .devcontainer/basics/datesjs.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+//let myCreateDate = new Date(2026,0,23)
+
+//let myCreateDate = new Date(2026,0,23,5,6)
+let myCreateDate = new Date("01-24-2025")
+//console.log(myCreateDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+
+
+
+
+
+
