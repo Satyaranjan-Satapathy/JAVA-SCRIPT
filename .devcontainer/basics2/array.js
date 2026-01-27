bbd7c5bee1f59a4bc1db7d6d5773d38b279e7e4d
Satyaranjan-Satapathy <satyaranjansatapathy48@gmail.com>
2026-01-27 14:12:48 +0000

    array basics done

---
+++ .devcontainer/basics2/array.js
@@ -0,0 +1,32 @@
+let myArr = [1,2,3,4,5]
+// console.log(myArr[2]);
+// console.log(myArr.length);
+
+
+// myArr.push(6)
+// //myArr.pop(6)
+// console.log(myArr);
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+
+//slice $ splice
+
+console.log("A",myArr);
+
+const mynew1 = myArr.slice(1,3)
+console.log(mynew1);
+console.log("B",myArr);
+
+const mynew2 = myArr.splice(1,3)
+console.log("C",myArr);
+console.log(mynew2);
+//this method manipulate the original array (splice method)
+
+
+
+
+
